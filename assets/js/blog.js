eb47922a1a49576af2fb9e2ecf3662c391509f77
thomas.dekeyser <thomas.dekeyser@gmail.com>
2018-03-12 21:35:20 +0100

    POC blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,29 +1,36 @@
-// Get tumblr post
-function getTumblr() {
-    console.log("Getting blog");
-    $.ajax({
-        type: 'GET',
-        url: "https://api.tumblr.com/v2/blog/utah-lot2.tumblr.com/posts?api_key=ekDkwXEZzdxqJNwEtwW64qRtmJW0LQjmgc7BI10W9w0Ywlo1qy",
-        async: false,
-        contentType: "application/json",
-        dataType: 'jsonp',
-        success: function(json) {
-            console.log(json);
-            var latest = json.response.posts[0];
-            $("#blog-headline").text(latest.title);
-            $("#blog-body").html(latest.body);
-            json.response.posts.forEach(function(el, i) {
-                if(i > 4) return false;
-                $("#blog-more").append("<li><a href='"+ el.post_url +"'>"+ el.title +"</li>");
-            });
-        },
-        error: function(e) {
-            console.log(e.message);
-        }
-    });
-}
-
-
-$(document).ready(function(e) {
-    getTumblr();
-});    
+var app = new Vue({
+    el: '#blogapp',
+    data: {
+      message: 'Hello Vue!',
+      posts: []    
+    },
+    created: function() {
+        console.log("Getting blog");
+        var that = this;
+        $.ajax({
+            type: 'GET',
+            url: "https://api.tumblr.com/v2/blog/utah-lot2.tumblr.com/posts?api_key=ekDkwXEZzdxqJNwEtwW64qRtmJW0LQjmgc7BI10W9w0Ywlo1qy",
+            async: false,
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function(json) {
+                console.log(json);
+                /*
+                var latest = json.response.posts[0];
+                $("#blog-headline").text(latest.title);
+                $("#blog-body").html(latest.body);
+                */
+                that.posts = json.response.posts;
+                /*
+                json.response.posts.forEach(function(el, i) {
+                    if(i > 4) return false;
+                    $("#blog-more").append("<li><a href='"+ el.post_url +"'>"+ el.title +"</li>");
+                });
+                 */
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+        });    
+    }
+})
